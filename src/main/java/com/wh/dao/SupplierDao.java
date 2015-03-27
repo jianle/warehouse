3f294c5bab370207836f1bca0a9dd4604703e18f
sabo loh <jianle@anjuke.com>
2015-03-27 19:10:59 +0800

    add SupplierDao

--- src/main/java/com/wh/dao/SupplierDao.java
+++ src/main/java/com/wh/dao/SupplierDao.java
@@ -1,5 +1,7 @@
 package com.wh.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -7,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.wh.model.Supplier;
 
@@ -29,27 +32,78 @@ public class SupplierDao implements BaseDao<Supplier, Long> {
 
     @Override
     public Supplier get(Long id) {
-        // TODO Auto-generated method stub
+        // 通过Id获取Supplier
         try {
             String sql = "SELECT " + SELECT_FIELDS + " FROM " + TABLE_NAME + " WHERE id=? ";
+            return jdbcTemplate.queryForObject(sql, rowMapper, id);
         } catch (Exception e) {
             // TODO: handle exception
+            logger.debug("Supplier get id failed ." + e);
+            return null;
+        }
+    }
+
+    @Override
+    public Boolean save(Supplier supplier) {
+        // 保存Supplier
+        try {//name, shortname, address, contact_name, contact_tel, is_disabled, insert_dt
+            String sql = "INSERT INTO " + TABLE_NAME + " (" + INSERT_FIELDS 
+                    + ") VALUES (?, ?, ?, ?, ?, ?, ?)";
+            jdbcTemplate.update(sql, supplier.getName(),
+                    supplier.getShortname(),
+                    supplier.getAddress(),
+                    supplier.getContactName(),
+                    supplier.getContactTel(),
+                    supplier.getIsDisabled(),
+                    supplier.getInsertDt()
+                    );
+            return true;
+        } catch (Exception e) {
+            // TODO: handle exception
+            logger.debug("Supplier save failed ." + e);
         }
         return null;
     }
 
     @Override
-    public Boolean save(Supplier object) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
     public List<Supplier> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        // 查找所有
+        try {
+            String sql = "SELECT " + SELECT_FIELDS + " FROM " + TABLE_NAME + " LIMIT 10000 ";
+            return jdbcTemplate.query(sql, rowMapper);
+        } catch (Exception e) {
+            // TODO: handle exception
+            logger.debug("Supplier findAll failed ." + e);
+            return null;
+        }
     }
     
     
+    
+    private RowMapper<Supplier> rowMapper = new RowMapper<Supplier>() {
+        
+        @Override
+        public Supplier mapRow(ResultSet rs, int rowNum) throws SQLException {
+            // TODO Auto-generated method stub
+            
+            Supplier supplier = new Supplier();
+            supplier.setsId(rs.getLong("s_id"));
+            supplier.setName(rs.getString("name"));
+            supplier.setShortname(rs.getString("shortname"));
+            supplier.setAddress(rs.getString("address"));
+            supplier.setContactName(rs.getString("contact_name"));
+            supplier.setContactTel(rs.getString("contact_tel"));
+            try {
+                supplier.setIsDisabled(rs.getString("is_disabled").toCharArray()[0]);
+            } catch (Exception e) {
+                logger.debug("Supplier is_disabled failed ." + e);
+            }
+            
+            supplier.setInsertDt(rs.getTimestamp("insert_dt"));
+            supplier.setUpdateTime(rs.getTimestamp("update_time"));
+            
+            return supplier;
+        }
+    };
 
 }
