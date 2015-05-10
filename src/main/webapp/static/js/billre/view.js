ae4aff9b37c6658e8df3a8c3b49f4910018bf9fb
sabo loh <jianle@anjuke.com>
2015-05-10 21:08:48 +0800

    billRecivable

--- src/main/webapp/static/js/billre/view.js
+++ src/main/webapp/static/js/billre/view.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
 	
 	$('#startDate').datepicker({
@@ -21,167 +23,66 @@ $(document).ready(function(){
 	
 });
 
-
-
-function modalFormCommit(){
-	addAndupdate();
-	//document.getElementById('modalForm').action= contextPath + '/billReceivable/save';
-//	$('.ui .form').from('validate form').onSuccess(function(){
-//		console.log('342342');
-//	});
-	//addAndupdate();
-}
-
+var url = contextPath;
 var flagTitle="编辑";
 var curDate = myformatter(new Date());
 $(document).ready(function() {
-	$('#closeModal').click(function(){
-		$('#modal').modal('hide');
-	});
-	
 	//初始化 Modal --by 
 	$('#addButton').click(function(){
-		$('#modal').modal('show');
-		
-		$('#brDate').datepicker({
-		    format: "yyyy-mm-dd",
-		    language: "zh-CN",
-		    orientation: "auto right",
-		    autoclose: true
-		});
-		
-		
+		//$('#modal').modal('show');
+		$('#dlg').dialog('open').dialog('setTitle','新增');
+		url = url + '/billReceivable/save';
 		if(flagTitle=="编辑"){
-			$('#modalTitle').html("新增");
-			$('#modal .content #customerCompany').removeAttr("readonly");
-			$('#modal .content #customerCompany').val("");
-			$('#modal .content #brDate').val("");
-			$('#modal .content #amount').val("");
-			$('#modal .content #remark').val("");
+			$('#fm').form('clear');
+			$('#brDate').datebox('setValue', curDate);
+			$('#amount').numberbox('setValue', 0);
+			$('#remark').textbox('setValue', '');
 		}
-		$('#brDate').datepicker('setDates',curDate);
 		flagTitle="新增";
 	});
 });
 
 
 function edit(Id){
-	document.getElementById('modalForm').action= contextPath + '/billReceivable/update';
-	$('#modal').modal('show');
-	$('#brDate').datepicker({
-	    format: "yyyy-mm-dd",
-	    language: "zh-CN",
-	    orientation: "auto right",
-	    autoclose: true
-	});
+	url = url + '/billReceivable/update';
+	$('#dlg').dialog('open').dialog('setTitle','编辑');
 	var trId = Id+"_tr";
 	flagTitle="编辑";
 	//初始化 Modal 
-	$('#modalTitle').html("编辑");
-	$('.content #brId').val(Id);
+	$('#brId').val(Id);
 	//$('.content #name').attr('readonly','readonly');
-	$('.content #customerCompany').val( $("#"+trId+" td")[1].innerHTML );
-	$('.content #brDate').val( $("#"+trId+" td")[2].innerHTML );
-	$('.content #amount').val( $("#"+trId+" td")[3].innerHTML );
-	$('.content #remark').val( $("#"+trId+" td")[4].innerHTML );
+	$('#customerCompany').textbox('setValue', $("#"+trId+" td")[1].innerHTML );
+	$('#brDate').datebox('setValue', $("#"+trId+" td")[2].innerHTML );
+	$('#amount').numberbox('setValue', $("#"+trId+" td")[3].innerHTML );
+	$('#remark').textbox('setValue', $("#"+trId+" td")[4].innerHTML );
 }
 
-//$(document).ready(function(){
-//	
-//	$('.ui.form')
-//	  .form({
-//		customerCompany: {
-//	      identifier  : 'customerCompany',
-//	      rules: [
-//	        {
-//	          type   : 'empty',
-//	          prompt : 'Please enter your name'
-//	        }
-//	      ]
-//	    },
-//	    brDate: {
-//	      identifier  : 'brDate',
-//	      rules: [
-//	        {
-//	          type   : 'empty',
-//	          prompt : 'Please select a gender'
-//	        }
-//	      ]
-//	    },
-//	    amount: {
-//	      identifier : 'amount',
-//	      rules: [
-//	        {
-//	          type   : 'empty',
-//	          prompt : 'Please enter a username'
-//	        }
-//	      ]
-//	    }
-//	  });
-//});
 
-
-function addAndupdate(){
-	console.log(contextPath);
-	var title = flagTitle;
-	if(title=="新增"){
-		document.getElementById('modalForm').action= contextPath + '/billReceivable/save';
-		$.ajax({
-			cache: true,
-			type:"POST",
-			url :contextPath + '/billReceivable/save',
-			data: $("#modalForm").serialize(),
-			async:true,
-			dataType: 'text',
-			success: function(result) {
-				if(result=='true'){
-					$.messager.confirm('提示', '添加成功，刷新页面查看', function(r){
-						if(r){
-							console.log('ok');
-							window.location.href=contextPath + '/billReceivable';
-						}
-					});
-				}else{
-					$.messager.alert("提示", "添加失败");
-				}
-				
-			},
-			error:function(result) {
-				$.messager.alert("提示", "添加失败");
-			}
-		});
-		$('#modal').modal('hide'); 
-		
-	}else if(title=="编辑"){
-		document.getElementById('modalForm').action= contextPath + '/billReceivable/update';
-		$.ajax({
-			cache: true,
-			type:"POST",
-			url :contextPath + '/billReceivable/update',
-			data: $("#modalForm").serialize(),
-			async:true,
-			dataType: 'text',
-			success: function(result) {
-				if(result=='true'){
-					$.messager.confirm('提示', '修改成功，刷新页面查看', function(r){
-						if(r){
-							console.log('ok');
-							window.location.href=contextPath + '/billReceivable';
-						}
-					});
-				}else{
-					$.messager.alert("提示", "修改失败");
-				}
-				
-			},
-			error:function(result) {
-				$.messager.alert("提示", "修改失败");
-			}
-		});
-	}
-	//document.getElementById('modalForm').submit();
-	$('#modal').modal('hide'); 
+function save(){
+	$('#modalForm').form('submit',{
+		url: url,
+		onSubmit: function(){
+            return $(this).form('validate');
+        },
+        dataType:'text',
+        success: function(data){
+        	var result = eval('('+data+')');
+            if (result){
+            	$('#dlg').dialog('close');
+                $.messager.confirm('提示', '操作成功，是否刷新页面查看', function(r){
+					console.log('ok');
+					window.location.href=contextPath + '/billReceivable';
+                });
+            } else {
+            	$.messager.show({
+                    title: '提示信息',
+                    msg: '操作失败'
+                });
+            }
+        }
+	}); 
 }
+
 
 function deleteInfo(brId){
 	$.messager.confirm('提示', '确认是否删除', function(r){
@@ -196,7 +97,10 @@ function deleteInfo(brId){
 				dataType: 'text',
 				success: function(result) {
 					if(result=='true'){
-						$.messager.alert("提示", "删除成功");
+						$.messager.show({
+		                    title: '提示信息',
+		                    msg: '删除成功'
+		                });
 						$("#"+brId+"_tr").remove()
 					}else{
 						$.messager.alert("提示", "删除失败");
